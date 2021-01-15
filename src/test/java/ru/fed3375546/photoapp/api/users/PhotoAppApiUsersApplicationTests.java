656836903328f04930c4c8607da891493bf47f16
Dmitriy <fed3375546@yandex.ru>
2021-01-15 12:11:46 +0300

    Users micriservice Eureka client

---
+++ src/test/java/ru/fed3375546/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fed3375546.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
