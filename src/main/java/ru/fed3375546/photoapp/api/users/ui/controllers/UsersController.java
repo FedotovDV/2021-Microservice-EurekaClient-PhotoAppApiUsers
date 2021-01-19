5ff4b11ab2c8968c3fe426d72b51ded71327cdf0
Dmitriy <fed3375546@yandex.ru>
2021-01-19 16:05:01 +0300

    add eureka.instance

--- src/main/java/ru/fed3375546/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/ru/fed3375546/photoapp/api/users/ui/controllers/UsersController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UsersController {
 
     @GetMapping("/status/check")
-    public String status(){
+    public String status() {
         return "Working";
     }
+
 }
